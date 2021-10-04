4ad8d1661aeec816d76d17d27fecd6c78fdd86b1
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-10-04 18:26:26 -0300

    Studio back to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Banner from "../components/Banner/Banner";
 import Portfolio from "../components/Portfolio/Portfolio";
-
+import Studio from "../components/Studio/Studio";
 const Home = () => {
 	return (
 		<>
 			<Banner></Banner>
 			<Portfolio></Portfolio>
+			<Studio></Studio>
 		</>
 	);
 };
