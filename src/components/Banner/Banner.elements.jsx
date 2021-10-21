73017c3347aa07ac12c56cb3a27aaff779922f56
Rodrigo <rodrigo1cabrera2@gmail.com>
2021-10-20 21:01:09 -0300

    Agregado .gif como reel

--- src/components/Banner/Banner.elements.jsx
+++ src/components/Banner/Banner.elements.jsx
@@ -1,11 +1,23 @@
 import styled from "styled-components";
 
 export const BannerContainer = styled.div`
-	/* position: relative; */
-	/* top: 180px; */
-	height: 70vh;
+	/* position: relative;
+	top: 180px; */
+	/* min-height: 60vh; */
 `;
 export const VideoBg = styled.iframe``;
+
+export const ReelContainer = styled.div`
+	position: relative;
+	overflow: hidden;
+	width: 100%;
+	height: 700px;
+`;
+export const ReelGif = styled.img`
+	object-fit: cover;
+	width: 100%;
+	height: 100%;
+`;
 export const BannerWrapper = styled.div`
 	height: 100%;
 	width: 100%;
--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,20 +6,26 @@ import {
 	// BannerTextContainer,
 	// BannerWrapper,
 	// BannerText,
-	VideoBg,
+	// VideoBg,
+	ReelGif,
+	ReelContainer,
 } from "./Banner.elements";
+import reel from "../../assets/ola-reel.gif";
 
 const Banner = () => {
 	return (
 		<BannerContainer>
 			<BannerWrapper>
-				<VideoBg
+				<ReelContainer>
+					<ReelGif src={reel} />
+				</ReelContainer>
+				{/* <VideoBg
 					width="100%"
 					height="100%"
 					src={`https://www.youtube.com/embed/ig54Pd5u5Zc?rel=0&showinfo=0&modestbranding=0&autoplay=1&controls=0&mute=1&loop=1&playlist=ig54Pd5u5Zc`}
 					title="YouTube video player"
 					allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-				></VideoBg>
+				></VideoBg> */}
 			</BannerWrapper>
 			{/* <BannerHeader>
 					<BannerTextContainer>
